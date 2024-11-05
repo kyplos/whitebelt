9b42fd8b122b938de668fd3317fcc75a83a209c0
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-11-05 13:16:46 +0100

    kata1
    a simple assignment but fun to look back at what we have done before

---
+++ kata1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace kata1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("welcome, brave adventurer! what is your name?");
+        
+        string name = Console.ReadLine();
+        
+        Console.WriteLine($"Greetings,{name} Your journey begins now... ");
+        
+        Console.WriteLine("on a scale of 1 to 10, how escited are you to start this quest?");
+        
+        int excitment = Convert.ToInt16(Console.ReadLine());
+
+        if (excitment >= 7)
+        {
+            Console.WriteLine("fantastic! you seem ready to take on any challenge");
+        }
+        else
+        {
+            Console.WriteLine("you dont seem to be excited to do this quest");
+        }
+    }
+}
