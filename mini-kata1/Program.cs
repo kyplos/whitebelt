b14df1f58670611b9b3cc6cb5cc285b9c117e4bc
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-11-07 15:01:32 +0100

    Variable Practice
    meh

---
+++ mini-kata1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace mini_kata1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string _name = "Kyplos";
+        int _healthPoints = 100;
+        float _attackPower = 16.75f;
+        bool _isParalzed = true;
+        double _currencyAmount = 55.75;
+        
+        Console.WriteLine($"Character Name: {_name}\n" +
+                          $"Health Points:{_healthPoints}\n" +
+                          $"Attack Power: {_attackPower}\n" +
+                          $"Is paralyzed: {_isParalzed}\n" +
+                          $"Gold Coins: {_currencyAmount}");
+        
+
+    }
+} 
