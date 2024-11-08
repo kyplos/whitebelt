224af0a1c0338847dae2f4da76dcdf044b8e882d
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-11-08 09:21:06 +0100

    kata 2
    fun and simple

---
+++ kata 2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace kata_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Kyplos";
+        int level = 7;
+        float healthPoints = 250;
+        double experiencePoints = 2534.24;
+        bool isMagical = true;
+        char rank = 'B';
+        double requiredExperience = 5000;
+        double experienceToLevel = requiredExperience - experiencePoints;
+        
+        Console.WriteLine($"Character Name:{name}");
+        Console.WriteLine($"Level:{level}");
+        Console.WriteLine($"Health Points:{healthPoints}");
+        Console.WriteLine($"Experience Points:{experiencePoints}");
+        Console.WriteLine($"Has Magical Abilities:{isMagical}");
+        Console.WriteLine($"Rank:{rank}");
+        Console.WriteLine($"Experience needed to level:{experienceToLevel}");
+    }
+}
