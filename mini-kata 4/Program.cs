dd3c7105c34cf4661decfb955d306bbc38973906
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-11-11 21:33:31 +0100

    mini-kata4
    i needed to think abit in the beginning but then it just flowed

---
+++ mini-kata 4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace mini_kata_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int spawnedEnemies = 0;
+        int maxEnemies = 3;
+        
+        Console.WriteLine("spawning enemies using a for loop:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"enemy #{i + 1} spawned!");
+        }
+        
+        Console.WriteLine("spawning enemies using a while loop:");
+        while (true)
+        {
+            if (spawnedEnemies >= maxEnemies)
+            {
+                break;
+            }
+            
+            Console.WriteLine($"enemy #{spawnedEnemies + 1} spawned!");
+            
+            spawnedEnemies ++;
+            
+        }
+    }
+}
