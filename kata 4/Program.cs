379a09015d2082e6c1fe1aad70968c6fdb1fef1b
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2024-11-11 21:37:14 +0100

    kata 4
    spawning waves of enemies

---
+++ kata 4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace kata_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int totalWaves = 2;
+        int enemiesPerWave = 5;
+        for (int wave = 1; wave <= totalWaves; wave++)
+        {
+            Console.WriteLine($"Wave {wave} is starting!\n");
+            for (int enemy = 1; enemy <= enemiesPerWave; enemy++)
+            {
+                
+                Console.WriteLine($"Enemy {enemy} has spawned.");
+                Thread.Sleep(500);
+            }
+            Thread.Sleep(250);
+            Console.WriteLine($"Wave {wave} completed. Preparing for the next wave...");
+        }       
+    }
+}
